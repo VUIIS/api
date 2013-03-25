a1570a482c8b0ef0de4e30f013209f7f9dc9694d
Scott Burns <scott.s.burns@gmail.com>
2013-03-25 16:31:14 -0500

    Add package

---
+++ cci/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" __init__.py
+
+Top-Level
+"""
+__author__ = 'VUIIS CCI <vuiis-cci@googlegroups.com>'
+__copyright__ = 'Copyright 2013 Vanderbilt University. All Rights Reserved'
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" setup.py
+
+Packaging for cci's python API
+"""
+
+
+from setuptools import setup
+
+# setup()
