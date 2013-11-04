cb87ea718e9c1c098fda7556693c34d4da37e9cc
bud42 <bdboyd42@gmail.com>
2013-11-04 11:20:14 -0600

    Add main update scripts, full_update updates all tasks, quick_update updates only open tasks

---
+++ cci/full_update.py
@@ -0,0 +1,23 @@
+import os, imp
+from datetime import datetime
+
+def parse_args():
+    from argparse import ArgumentParser
+    ap = ArgumentParser(prog='full_update', description="Updates all tasks")
+    ap.add_argument(dest='settings_path', help='Settings Path')
+    return ap.parse_args()
+
+if __name__ == '__main__':   
+    args = parse_args()
+    settings_path = args.settings_path
+
+    print('INFO:running full update, Start Time:'+str(datetime.now()))
+
+    # Load the settings file
+    print('INFO:loading settings from:'+settings_path)
+    settings = imp.load_source('settings', settings_path)
+
+    # Run the full update
+    settings.myLauncher.update()
+
+    print('INFO:finished full update, End Time: ', str(datetime.now()))
---
+++ cci/quick_update.py
@@ -0,0 +1,23 @@
+import os, imp
+from datetime import datetime
+
+def parse_args():
+    from argparse import ArgumentParser
+    ap = ArgumentParser(prog='quick_update', description="Updates only open tasks")
+    ap.add_argument(dest='settings_path', help='Settings Path')
+    return ap.parse_args()
+
+if __name__ == '__main__':   
+    args = parse_args()
+    settings_path = args.settings_path
+
+    print('INFO:running quick update, Start Time:'+str(datetime.now()))
+
+    # Load the settings file
+    print('INFO:loading settings from:'+settings_path)
+    settings = imp.load_source('settings', settings_path)
+
+    # Run the quick update
+    settings.myLauncher.update_open_tasks()
+
+    print('INFO:finished quick update, End Time: ', str(datetime.now()))
