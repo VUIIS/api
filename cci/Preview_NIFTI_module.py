d086ea08574edff68c9ba4b4327aa918abc54c24
byvernault <byvernault@gmail.com>
2013-12-11 12:13:32 -0600

    Adding a variable resourcename, the preview can be on a resource different that NIFTI. By default NIFTI

--- cci/Preview_NIFTI_module.py
+++ cci/Preview_NIFTI_module.py
@@ -6,11 +6,13 @@ DEFAULT_TPM_PATH='/tmp/Preview_NIFTI_tmp/'
 DEFAULT_MODULE_NAME='Preview_NIFTI'
 DEFAULT_TEXT_REPORT='ERROR/WARNING for preview NIFTI :\n'
 DEFAULT_EMAIL='nan'
+DEFAULT_RESOURCE='NIFTI'
 
 class Preview_NIFTI_Module(ScanModule):
-    def __init__(self,module_name=DEFAULT_MODULE_NAME,directory=DEFAULT_TPM_PATH,email=DEFAULT_EMAIL,Text_report=DEFAULT_TEXT_REPORT,masimatlabpath=DEFAULT_MASIMATLAB_PATH):
+    def __init__(self,module_name=DEFAULT_MODULE_NAME,directory=DEFAULT_TPM_PATH,email=DEFAULT_EMAIL,Text_report=DEFAULT_TEXT_REPORT,masimatlabpath=DEFAULT_MASIMATLAB_PATH,resourcename=DEFAULT_RESOURCE):
         super(Preview_NIFTI_Module, self).__init__(module_name,directory,email,Text_report=DEFAULT_TEXT_REPORT)
         self.masimatlabpath=masimatlabpath
+        self.resourcename=resourcename
     
     def prerun(self):
         #make directory
@@ -24,24 +26,24 @@ class Preview_NIFTI_Module(ScanModule):
         if Scan.resource('SNAPSHOTS').exists():
             print "      -Has snapshot."
         else:
-            if Scan.resource('NIFTI').exists():
-                if len(Scan.resource('NIFTI').files().get()) > 0:
-                    print "      -downloading NIFTI..."
+            if Scan.resource(self.resourcename).exists():
+                if len(Scan.resource(self.resourcename).files().get()) > 0:
+                    print "      -downloading "+self.resourcename+"..."
                     
-                    dl,NIFTI_filename=XnatUtils.download_biggest_resources(Scan.resource('NIFTI'),self.directory)
+                    dl,filename=XnatUtils.download_biggest_resources(Scan.resource(self.resourcename),self.directory)
                     
                     if not dl:
-                        print '      -ERROR: NIFTI file size is zero.'
+                        print '      -ERROR: '+self.resourcename+' file size is zero.'
                     else:
-                        smGif = os.path.join(self.directory,os.path.splitext(os.path.basename(NIFTI_filename))[0] + "_sm.gif")
-                        lgGif = os.path.join(self.directory,os.path.splitext(os.path.basename(NIFTI_filename))[0] + "_lg.gif")
+                        smGif = os.path.join(self.directory,os.path.splitext(os.path.basename(filename))[0] + "_sm.gif")
+                        lgGif = os.path.join(self.directory,os.path.splitext(os.path.basename(filename))[0] + "_lg.gif")
                         
                         #Matlab code to generate preview
                         f = open(self.directory+'/callpreview.m', "w")
                         try:
                             lines=[ 'addpath(genpath(\'' + self.masimatlabpath + '/trunk/xnatspiders/matlab/nii2gif/\'));\n',
                                     'addpath(genpath(\'' + self.masimatlabpath + '/trunk/xnatspiders/matlab/ext/\'));\n',
-                                    'src = \'' + NIFTI_filename + '\'\n',
+                                    'src = \'' + filename + '\'\n',
                                     'sm = \'' + smGif + '\'\n',
                                     'lg = \'' + lgGif + '\'\n',
                                     'makeNiiGifPreview(src,sm,lg)\n',
@@ -57,7 +59,7 @@ class Preview_NIFTI_Module(ScanModule):
                         print '===================================================================\n'
                         
                         if os.path.isfile(smGif) : 
-                            print '      -GIF Made'
+                            print '      -GIF Made / upload ...'
                             Scan.resource('SNAPSHOTS').create()
                             Scan.resource('SNAPSHOTS').file('snap_t.gif').put(smGif,'GIF','THUMBNAIL');
                             Scan.resource('SNAPSHOTS').file('snap.gif').put(lgGif,'GIF','ORIGINAL');
@@ -72,4 +74,4 @@ class Preview_NIFTI_Module(ScanModule):
                 else:
                     print "    *ERROR : The size of the resource is 0."      
             else:
-                print '    *No nifti.'
+                print '    *No resource '+self.resourcename
